9c3b10d4ed2b5e313d7b8017d15bd0c87630823f
Mweh <fahmianton@gmail.com>
2022-08-06 17:34:03 +0800

    v1.3

---
+++ BMI Calculator/Controllers/CalculatorBrain.swift
@@ -0,0 +1,39 @@
+//
+//  CalculatorBrain.swift
+//  BMI Calculator
+//
+//  Created by Muhammad Fahmi on 06/08/22.
+//  Copyright © 2022 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct CalculatorBrain{
+    var bmi: BMI?
+    
+    func getBMIValue() -> String {
+        let bmiTo1DecimalPlace = String(format: "%1.f", bmi?.value ?? 0.0)
+        return bmiTo1DecimalPlace
+    }
+    
+    func getAdvice() -> String{
+        return bmi?.advice ?? "Nil"
+    }
+    
+    func getColor() -> UIColor{
+        return bmi?.color ?? .systemGray
+    }
+    
+    mutating func calculateBMI(height: Float, weight: Float){
+        let bmiValue = weight/pow(height, 2)
+        
+        if bmiValue < 18.5{
+            bmi = BMI(value: bmiValue, advice: "Eat more pies!", color: .systemBlue)
+        } else if bmiValue < 24.9{
+            bmi = BMI(value: bmiValue, advice: "Fit as a fiddle!", color: .systemGreen)
+        } else {
+            bmi = BMI(value: bmiValue, advice: "Eat less pies!", color: .systemRed)
+        }
+    }
+    
+}
